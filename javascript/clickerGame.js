17df380b4b85fa47b4e3419e74cd57e12aa18659
zachgalbs <zacharythomasgalbraith@gmail.com>
2022-10-12 11:20:45 -0700

    Re-write and new features
    I re-wrote all of the code from clickerGame.js and clickerGame2.js to make it cleaner and more readable. I also added a dialogue feature and made the button bouncy. Changed the layout too.

--- javascript/clickerGame.js
+++ javascript/clickerGame.js
@@ -1,63 +1,97 @@
-let num = 0;
-let increaseValue = 1
-let upgradeWorth = 1
-const htmlNum = document.getElementById("score");
-const increaseButton = document.getElementById("increase");
-const decreaseButton = document.getElementById("decrease");
-const resetButton = document.getElementById("reset");
-const upgradeButton = document.getElementById("buyingOption");
-const rebirthButton = document.getElementById("rebirthButton");
-let rebirthMultiplier = 1;
-let allowUpgrade = false;
-let allowRebirth = false;
+let money = 0;
+let increaseValue = .1
+let upgradeWorth = 0
+let upgradePrice = 0
 
-// This is all the code for making money
-increaseButton.onclick = function() {
-    if (num < 180 && (num + increaseValue >= 180)) {
-        upgradeButton.style.opacity = "1";
-        upgradeButton.style.cursor = "pointer";
-        allowUpgrade = true
-        upgradeButton.innerHTML = "Buy a cheap violin! (+2 per click)"
-        upgradeWorth = 2;
+let htmlMoney;
+let increaseButton;
+let upgradeButton;
+let dialogue;
+
+
+let upgradeStack = [];
+
+function setupGame() {
+    htmlMoney = document.getElementById("score");
+    increaseButton = document.getElementById("increase");
+    upgradeButton = document.getElementById("buyingOption");
+    dialogue = document.getElementById("dialogue");
+    runGame()
+}
+
+function runGame() {
+    increaseButton.onclick = function() {
+        upgradeCheck();
+        money += increaseValue;
+        money = Math.round(money*100)/100
+        htmlMoney.innerHTML = money;
+        htmlMoney.innerHTML = "$" + money;
+        checkForDialogue();
     }
-    else if (num < 60 && (num + increaseValue >= 60)) {
-        upgradeButton.style.opacity = "1";
-        upgradeButton.style.cursor = "pointer";
-        allowUpgrade = true
-        upgradeButton.innerHTML = "Buy a bucket and a drum stick! (+1 per click)"
-    }
-    else if (num < 30 && (num + increaseValue >= 30)) {
-        upgradeButton.style.opacity = "1";
-        upgradeButton.style.cursor = "pointer";
-        allowUpgrade = true
-        upgradeButton.innerHTML = "Buy a water bottle! (+$1 per click)"
-    }
-    num += increaseValue;
-    htmlNum.innerHTML = num;
-    // This is all the code for the rebirth feature:
-    if (num >= 500 * rebirthMultiplier) {
-        rebirthButton.style.opacity = "1";
-        rebirthButton.style.cursor = "pointer";
-        allowRebirth = true;
+    upgradeButton.onclick = function() {
+        popUpgrade();
     }
 }
 
-upgradeButton.onclick = function() {
-    checkUpgrade()
+function upgradeCheck() {
+    // WATER BOTTLE
+    if (moneyPassed(10) && increaseValue == 0.1) {pushUpgrade({text: "Buy a nourishing meal! (+$0.02 per click, -$7.00)", value: 0.02, price: 7})};
+    // DRUMSTICK 
+    console.log(increaseValue);
+    if (moneyPassed(25) && increaseValue == 0.12) {pushUpgrade({text: "Buy a Bucket and Drum Sticks! (+$0.04 per click, -$15.00)", value: 0.04, price: 15})}
+    // VIOLIN
+    if (moneyPassed(50) && increaseValue == 0.16) {pushUpgrade({text: "Buy a cheap violin and some sheet music! (+$0.04 per click, -$45.00)", value: 0.04, price: 45})}
 }
-rebirthButton.onclick = function() {
-    if (allowRebirth) {
-    rebirthMultiplier += .2;
-    rebirthButton.style.opacity = "0";
-    rebirthButton.style.cursor = "default";
+
+function moneyPassed(neededMoney) {
+    return (money < neededMoney) && (money + increaseValue >= neededMoney)
+}
+
+function pushUpgrade(upgrade) {
+    upgradeStack.push(upgrade);
+    showUpgradeStack();
+}
+
+function popUpgrade() {
+    if (upgradeStack.length > 0) {
+        increaseValue += upgradeWorth;
+        money -= upgradePrice;
+        htmlMoney.innerHTML = "$" + money.toLocaleString("en-US");
     }
+    upgradeStack.pop();
+    showUpgradeStack();
 }
-function checkUpgrade() {
-    if (allowUpgrade){
+
+function showUpgradeStack() {
+    if (upgradeStack.length > 0) {
+        let recentUpgrade = upgradeStack[upgradeStack.length -1];
+        //Set the text and upgrade worth
+        upgradeButton.innerHTML = recentUpgrade.text;
+        upgradeWorth = recentUpgrade.value;
+        upgradePrice = recentUpgrade.price;
+        // Reveal the Button
+        upgradeButton.style.opacity = "1";
+        upgradeButton.style.cursor = "pointer";
+    }
+    else {
+        //Hide the button
         upgradeButton.style.opacity = "0";
         upgradeButton.style.cursor = "default";
-        increaseValue += upgradeWorth * rebirthMultiplier;
-        console.log("upgraded!")
     }
-    allowUpgrade = false;
+}
+
+function checkForDialogue() {
+    console.log(money);
+    if (money >= 120) {
+        dialogue.innerHTML = "Your homeless buddies start asking you for advice on making money!";
+    }
+    else if (money >= 40) {
+        dialogue.innerHTML = "You know some people around the city that come to your performances!";
+    }
+    else if (money >= 6) {
+        dialogue.innerHTML = "You've found some homeless buddies!";
+    }
+    else if (moneyPassed) {
+        dialogue.innerHTML = "You're now very poor on the streets of Palo Alto, California";
+    }
 }
